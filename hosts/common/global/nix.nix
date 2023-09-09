44a711f1cc1c9f645d6579ee79c00f7f5a4f0378
narinari <narinari.t@gmail.com>
2023-09-09 17:08:40 +0900

    Enable repl-flake

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,11 +8,14 @@
       # Deduplicate and optimize nix store
       auto-optimise-store = true;
 
-      substituters = [ "https://cache.nixos.org/" ];
-      trusted-public-keys =
-        [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+      substituters =
+        [ "https://cache.nixos.org/" "https://nix-community.cachix.org/" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
 
-      trusted-users = [ "@admin" ];
+      trusted-users = [ "@admin" "narinari" ];
     };
 
     # Add each flake input as a registry
@@ -29,6 +32,7 @@
 
     extraOptions = lib.optionalString (pkgs.system == "aarch64-darwin") ''
       extra-platforms = aarch64-darwin
+      builders = ssh-ng://builder@linux-builder aarch64-linux /etc/nix/builder_ed25519 4 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=
     '';
     #   extra-platforms = x86_64-darwin aarch64-darwin
     # '';
