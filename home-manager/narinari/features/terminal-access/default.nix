238f9fe880b20287bfd064c594b51cb925fafdc0
narinari <narinari.t@gmail.com>
2023-10-22 15:32:02 +0900

    :bug: Fix alt command option

--- home-manager/narinari/features/terminal-access/default.nix
+++ home-manager/narinari/features/terminal-access/default.nix
@@ -2,7 +2,7 @@
 
 {
   home.sessionVariables = {
-    EDITOR = "emacsclient -nw -a emacs -nw";
-    VISUAL = "emacsclient -nw -a emacs -nw";
+    EDITOR = "emacsclient -nw -a 'emacs -nw'";
+    VISUAL = "emacsclient -nw -a 'emacs -nw'";
   };
 }
