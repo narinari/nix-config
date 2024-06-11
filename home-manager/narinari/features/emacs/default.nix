88792c9f958bc285aaa97bf52493208aec9beab3
narinari <narinari.t@gmail.com>
2024-06-11 11:00:38 +0900

    :bug: Fix emacs patch apply timing

--- home-manager/narinari/features/emacs/default.nix
+++ home-manager/narinari/features/emacs/default.nix
@@ -6,9 +6,36 @@ let
     repoUrl = "https://github.com/doomemacs/doomemacs";
     configRepoUrl = "git@github.com.private:narinari/doom.git";
   };
-  customEmacs =
-    (pkgs.emacsPackagesFor pkgs.emacs-unstable-pgtk).emacsWithPackages
-    (epkgs: with epkgs; [ vterm treesit-grammars.with-all-grammars ]);
+  patchedEmacs = pkgs.emacs-unstable-pgtk.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ optional stdenv.isDarwin [
+      # Fix OS window role (needed for window managers like yabai)
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+        sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+      })
+      # Use poll instead of select to get file descriptors
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
+        sha256 = "jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
+      })
+      # Enable rounded window with no decoration
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+        sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+      })
+      # Make Emacs aware of OS-level light/dark mode
+      (pkgs.fetchpatch {
+        url =
+          "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+        sha256 = "oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
+      })
+    ];
+  });
+  customEmacs = (pkgs.emacsPackagesFor patchedEmacs).emacsWithPackages
+    (epkgs: [ epkgs.vterm ]);
 in {
   home = {
     packages = with pkgs; [
@@ -53,33 +80,6 @@ in {
 
   programs.emacs = {
     enable = true;
-    package = customEmacs.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ optional stdenv.isDarwin [
-        # Fix OS window role (needed for window managers like yabai)
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
-          sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
-        })
-        # Use poll instead of select to get file descriptors
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-          sha256 = "jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY=";
-        })
-        # Enable rounded window with no decoration
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
-          sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
-        })
-        # Make Emacs aware of OS-level light/dark mode
-        (pkgs.fetchpatch {
-          url =
-            "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
-          sha256 = "oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8=";
-        })
-      ];
-    });
+    package = patchedEmacs;
   };
 }
