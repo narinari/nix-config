18b79aad78e370c6349e46d12b1059d056da2471
narinari <narinari.t@gmail.com>
2025-02-07 19:17:02 +0900

    update nixfmt to rfc-style

--- nix/checks.nix
+++ nix/checks.nix
@@ -1,4 +1,9 @@
-{ pkgs, pre-commit-hooks, deploy-rs, ... }:
+{
+  pkgs,
+  pre-commit-hooks,
+  deploy-rs,
+  ...
+}:
 
 system:
 
@@ -10,7 +15,7 @@ with pkgs;
     hooks = {
       actionlint.enable = true;
       # luacheck.enable = true;
-      nixfmt = {
+      nixfmt-rfc-style = {
         enable = true;
         excludes = [ "hardware-configuration.*.nix" ];
       };
@@ -22,23 +27,22 @@ with pkgs;
           "home-manager/narinari/work/work-config/aws.zsh"
         ];
       };
-      statix.enable = true;
+      statix = {
+        enable = true;
+        settings.ignore = [
+          "hosts/common/users/narinari/default.nix" # BUG: Statix throws parsing error if parameter expansion exists in path.
+        ];
+      };
       # stylua.enable = true;
       git-secrets = {
         enable = true;
         name = "Git Secrets";
-        description =
-          "git-secrets scans commits, commit messages, and --no-ff merges to prevent adding secrets into your git repositories.";
+        description = "git-secrets scans commits, commit messages, and --no-ff merges to prevent adding secrets into your git repositories.";
         entry = "${git-secrets}/bin/git-secrets --pre_commit_hook";
         language = "script";
       };
-
     };
 
-    settings = {
-      statix.ignore = [
-        "hosts/common/users/narinari/default.nix" # BUG: Statix throws parsing error if parameter expansion exists in path.
-      ];
-    };
   };
-} // (deploy-rs.lib.${system}.deployChecks self.deploy)
+}
+// (deploy-rs.lib.${system}.deployChecks self.deploy)
