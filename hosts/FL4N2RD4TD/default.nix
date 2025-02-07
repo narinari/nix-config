167994c23b6e0bb558d3bf4a9a21410a767b0b39
narinari <narinari.t@gmail.com>
2025-02-07 19:19:20 +0900

    Set system.stateVersion

--- hosts/FL4N2RD4TD/default.nix
+++ hosts/FL4N2RD4TD/default.nix
@@ -1,9 +1,17 @@
 { pkgs, inputs, ... }:
 
 {
-  imports = [ ../common/global ../common/darwin ../common/users/narinari ];
+  imports = [
+    ../common/global
+    ../common/darwin
+    ../common/users/narinari
+  ];
 
-  networking = { hostName = "FL4N2RD4TD"; };
+  networking = {
+    hostName = "FL4N2RD4TD";
+  };
+
+  system.stateVersion = 4;
 
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
