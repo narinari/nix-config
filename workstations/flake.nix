19d6c0b4460ac78d9bb96cb8cb3049de1aacf27a
narinari <narinari.t@gmail.com>
2026-06-11 08:56:45 +0900

    chore(flake): change nixpkgs branch

--- workstations/flake.nix
+++ workstations/flake.nix
@@ -2,7 +2,7 @@
   description = "Workstations - desktop and Darwin machines (nixpkgs master)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -10,7 +10,7 @@
     };
 
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
